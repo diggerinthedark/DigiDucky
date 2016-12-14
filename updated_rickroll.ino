1b7ff509a5a99dcc33fabc1e67d1742cae8ebe00
Digger <bigmupps@gmail.com>
2016-12-14 10:17:46 +0000

    updated streamlined a bit (#1)
    * updated streamlined a bit
    
    not functioning on win 8.1 or win10 when i tested so ive sorted it out a bit and updated some stuff
    
    * Update updated_rickroll.ino
    
    * changed link for ad free (i think)
    
    * kiosk mode, because trolling.

---
+++ updated_rickroll.ino
@@ -0,0 +1,42 @@
+#include "DigiKeyboard.h"
+
+int iterationCounter = 0;
+
+void setup() {
+  
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);   
+  digitalWrite(0, LOW);   
+  digitalWrite(1, LOW); 
+  
+}
+
+
+void loop() {
+  
+  DigiKeyboard.update();
+
+  if (iterationCounter == 0) {
+    DigiKeyboard.sendKeyStroke(0);
+  
+  
+    DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT); //minimise all windows
+  DigiKeyboard.delay(1000);  
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_RIGHT); //open run dialogue
+  
+ DigiKeyboard.delay(1000);
+ DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.print("iexplore -k https://www.youtube.com/watch?v=oHg5SJYRHA0"); //point to iexplore, tell it to launch in kiosk mode and pass rick astley link
+   DigiKeyboard.delay(1000);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //press ok
+  
+    
+   }
+
+  delay(1000);
+  
+  iterationCounter++;
+}
